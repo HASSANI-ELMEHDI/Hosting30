27de9b92b61ff371e3f5ca9b7e5576846297c816
HASSANI-ELMEHDI <105174552+HASSANI-ELMEHDI@users.noreply.github.com>
2024-01-09 19:30:24 +0100

    Date picker added

--- app/reserving/[id].tsx
+++ app/reserving/[id].tsx
@@ -1,20 +1,41 @@
 import { useLocalSearchParams, useNavigation } from 'expo-router';
 import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions, TouchableOpacity, Share, ScrollView, FlatList } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions, TouchableOpacity, Share, ScrollView, FlatList, Button } from 'react-native';
 
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
 import Colors from '@/constants/Colors';
 import { defaultStyles } from '@/constants/Styles';
 import { fetchData } from '@/constants/api';
-
+import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
+import * as Haptics from "expo-haptics";
+import CalendarPicker from 'react-native-calendar-picker';
 const { width } = Dimensions.get('window');
 const IMG_HEIGHT = 300;
+const AnimatedTouchableOpacity =
+	Animated.createAnimatedComponent(TouchableOpacity);
 const DetailsPage = () => {
 
- 
 
+  
+  const [openCard, setOpenCard] = useState(0);
   const { id } = useLocalSearchParams();
   const [listingsData , setData] = useState([]);
+  const [Nbrtenants, setNbrtenants] = useState(1);
+  const [selectedStartDate, setSelectedStartDate] = useState(null);
+  const [selectedEndDate, setSelectedEndDate] = useState(null);
+  const startDate  =  selectedStartDate ? selectedStartDate.toString() : '';
+    const endDate = selectedEndDate ? selectedEndDate.toString() : '';
+  const onDateChange = (date, type) => {
+    if (type === 'END_DATE') {
+      setSelectedEndDate(date);
+    } else {
+      setSelectedStartDate(date);
+      setSelectedEndDate(date);
+    }
+  }
+
+  const minDate = new Date(); // Todo : min from database
+  const maxDate = new Date(2024, 0, 15); // Todo : max from database
 
   useEffect(() => {
     fetchData()
@@ -25,20 +46,11 @@ const DetailsPage = () => {
   const listing = (listingsData as any[]).find((item) => item._id === id);
   const navigation = useNavigation();
 
-  const shareListing = async () => {
-    try {
-      await Share.share({
-        title: listing?.name,
-        url: listing?.listing_url,
-      });
-    } catch (err) {
-      console.log(err);
-    }
-  };
+
   useLayoutEffect(() => {
     navigation.setOptions({
       headerTitle: 'Confirm reservation',
-      headerTransparent: true,
+      headerTransparent: false,
 
       headerBackground: () => (
         <View style={[styles.header]}></View>
@@ -55,13 +67,135 @@ const DetailsPage = () => {
 
 
   return (
-    <View style={styles.container}>
-      <ScrollView
-        contentContainerStyle={{ paddingBottom: 100 }}
-        scrollEventThrottle={16}>
-      
+    <View style={styles.container} >
+			<ScrollView
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={{ paddingBottom: 100 }}
+			>
+         	{/* When */}
+				<View style={[stylesBody.card]}>
+					{openCard != 1 ? (
+						<AnimatedTouchableOpacity
+							style={stylesBody.cardPreview}
+							onPress={() => setOpenCard(1)}
+							entering={FadeIn.duration(200)}
+							exiting={FadeOut.duration(200)}
+						>
+							<Text style={stylesBody.previewText}>When</Text>
+							<Text style={stylesBody.previewDate}>Choose</Text>
+						</AnimatedTouchableOpacity>
+					) : (
+						<>
+							<Animated.Text
+								style={stylesBody.cardHeader}
+								entering={FadeIn.delay(100)}
+							>
+								When's your trip?
+							</Animated.Text>
 
-         
+							<Animated.View
+								style={[stylesBody.cardBody]}
+								entering={FadeIn.delay(100)}
+							>
+                             <CalendarPicker
+                               previousComponent ={<MaterialIcons name="navigate-before" size={24} color={Colors.primary} />}
+                                nextComponent ={<MaterialIcons name="navigate-next" size={24} color={Colors.primary} />}
+                                startFromMonday={true}
+                                allowRangeSelection={true}
+                                minDate={minDate}
+                                maxDate={maxDate}
+                                selectedDayColor={Colors.yellow}
+								 selectedDayTextColor="#FFFFFF" 
+								 onDateChange={onDateChange} />
+							</Animated.View>
+						</>
+					)}
+				</View>
+        	{/* Who */}
+				<View style={stylesBody.card}>
+						<Text style={stylesBody.cardHeader}>
+								Who's coming?
+						</Text>
+						{/*<Text style={stylesBody.capacityText}>
+								The maximum capacity of this accommodation is limited to {listing?.accommodates} occupants.
+					</Text>*/}
+                       <View style={[stylesBody.cardBody, { paddingHorizontal: 20 }]}>
+									<View style={[stylesBody.guestItem]}>
+										<View>
+											<Text style={{ fontSize: 14, fontFamily: "mon-sb" }}>
+                                              Number of people
+											</Text>
+											<Text
+												style={{
+													fontSize: 14,
+													fontFamily: "mon",
+													color: Colors.grey,
+												}}
+											>
+												{Nbrtenants} tenant(s)
+											</Text>
+										</View>
+
+										<View
+											style={{
+												flexDirection: "row",
+												gap: 10,
+												justifyContent: "center",
+												alignItems: "center",
+											}}
+										>
+											<TouchableOpacity
+												onPress={() => {
+													if (Nbrtenants > 0)
+														Haptics.impactAsync(
+															Haptics.ImpactFeedbackStyle.Light
+														);
+													setNbrtenants(Nbrtenants > 1 ? Nbrtenants - 1 : 1);
+												}}
+											>
+												<Ionicons
+													name="remove-circle-outline"
+													size={26}
+													color={
+														Nbrtenants > 1 ? Colors.grey : "#dedede"
+													}
+												/>
+											</TouchableOpacity>
+
+											<Text
+												style={{
+													minWidth: 18,
+													fontSize: 17,
+													fontFamily: "mon",
+													textAlign: "center",
+												}}
+											>
+												{Nbrtenants}
+											</Text>
+
+											<TouchableOpacity
+												onPress={() => {
+                                                   setNbrtenants(Nbrtenants < listing?.accommodates ? Nbrtenants + 1 : listing?.accommodates);
+													Haptics.impactAsync(
+														Haptics.ImpactFeedbackStyle.Light
+													);
+												}}
+											>
+												<Ionicons
+													name="add-circle-outline"
+													size={26}
+													color={
+														Nbrtenants === listing?.accommodates ? "#dedede" : Colors.grey 
+													}
+												/>
+											</TouchableOpacity>
+										</View>
+									</View>
+							
+							</View>
+							
+		
+				</View>
 
 
        </ScrollView>
@@ -76,18 +210,25 @@ const DetailsPage = () => {
             <Text style={styles.footerPrice}>€{listing?.price}</Text>
             <Text style={{color:Colors.primary}}> / per night</Text>
             </Text>
-            <Text style={{color:Colors.dark,textDecorationLine :'underline'}}> 8-13 janv</Text>
-            <Text style={{color:Colors.dark}}> 2 Persons</Text>
+            { ( selectedStartDate && selectedEndDate ) && (
+                     <Text style={{color:Colors.dark,textDecorationLine :'underline'}}>
+                      { startDate.split(" ")[1] } { startDate.split(" ")[2]} - 
+                      { endDate.split(" ")[1] } { endDate.split(" ")[2] }
+                    </Text>
+            )}
+           
+            <Text style={{color:Colors.dark}}> {Nbrtenants} Tenant(s)</Text>
     
             </View>
           </TouchableOpacity>
           
-          <TouchableOpacity style={[defaultStyles.btn,  { paddingRight: 20,backgroundColor: Colors.yellow, paddingLeft: 20 },]}>
+          <TouchableOpacity style={[defaultStyles.btn,  { paddingRight: 20,backgroundColor: Colors.primary, paddingLeft: 20 },]}>
             <Text style={defaultStyles.btnText}>Confirm </Text>
           </TouchableOpacity>
         </View>
       </View>
     </View>
+    
   );
 };
 
@@ -179,6 +320,102 @@ const styles = StyleSheet.create({
     marginTop: 10,
     fontFamily: 'mon',
   },
+});
+
+
+
+const stylesBody = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingTop: 100,
+		alignItems: "center",
+	},
+	card: {
+		margin: 10,
+		gap: 20,
+		backgroundColor: "#fff",
+		borderRadius: 14,
+		elevation: 4,
+		shadowColor: "#000",
+		shadowOpacity: 0.3,
+		shadowRadius: 4,
+		shadowOffset: {
+			width: 2,
+			height: 2,
+		},
+	},
+	cardHeader: {
+		padding: 20,
+		fontSize: 16,
+		fontFamily: "mon-b",
+	},
+	cardBody: {
+		alignItems: "center",
+	},
+	cardPreview: {
+		padding: 20,
+		flexDirection: "row",
+		justifyContent: "space-between",
+	},
+	searchSection: {
+		height: 50,
+		marginHorizontal: 20,
+		marginBottom: 25,
+		flexDirection: "row",
+		alignItems: "center",
+		borderWidth: 1,
+		borderColor: "#ababab",
+		borderRadius: 8,
+	},
+	inputField: {
+		flex: 1,
+		padding: 10,
+		backgroundColor: "#fff",
+	},
+	searchIcon: {
+		padding: 10,
+	},
+	place: {
+		width: 120,
+		height: 120,
+		borderRadius: 10,
+	},
+	placeSelected: {
+		width: 120,
+		height: 120,
+		borderWidth: 2,
+		borderColor: Colors.grey,
+		borderRadius: 10,
+	},
+	guestItem: {
+		width: "100%",
+		paddingVertical: 16,
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+	},
+	guestItemBorder: {
+		borderBottomWidth: StyleSheet.hairlineWidth,
+		borderBottomColor: Colors.grey,
+	},
+	previewText: {
+		color: Colors.grey,
+		fontSize: 14,
+		fontFamily: "mon-sb",
+	},
+	previewDate: {
+		color: Colors.dark,
+		fontSize: 14,
+		fontFamily: "mon-sb",
+    textDecorationLine : 'underline'
+	},
+  capacityText  : {
+    color: Colors.grey,
+		fontSize: 14,
+    marginTop : 50,
+		fontFamily: "mon",
+
+  }
 });
 
 export default DetailsPage;
